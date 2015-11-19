897cb55141902119a94c2de2e7d54661de808529
SAC-CS112-NGUYEN-TUAN <tmacnguyen0525@gmail.com>
2015-11-18 20:24:26 -0800

    ...

--- RollingDices/src/Dice.java
+++ RollingDices/src/Dice.java
@@ -24,4 +24,6 @@ public class Dice {
 		}
 		return randomnum;
 	}
+	
+	
 }
--- RollingDices/src/main.java
+++ RollingDices/src/main.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
 		Dice throwing = new Dice();
 		Dice boucing = new Dice();
+		Scanner input = new Scanner(System.in);
 		
-		System.out.println(throwing.Throw(1));
-		System.out.println(throwing.Throw(2,8));
-
+		
+		System.out.println("Insert bounces :");
+			int b = input.nextInt();
+		
+		System.out.println("Insert dices :");
+			int d = input.nextInt();	
+		if (d == 0){
+				System.out.println(throwing.Throw(b));
+		}
+		else{
+		System.out.println(throwing.Throw(d,b));
+		}
 	}
 
 }
